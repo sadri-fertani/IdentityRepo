27e8fd57814ea47c459cb9447ead16e677a970b6
sadri fertani <sadri.fertani@live.fr>
2019-08-27 02:46:28 +0100

    changes 524/230

---
+++ IdentityServer/AuthServer.Infrastructure/Data/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Infrastructure.Data.Identity
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ IdentityServer/AuthServer.Infrastructure/Services/IdentityClaimsProfileService.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AuthServer.Infrastructure.Data.Identity;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthServer.Infrastructure.Services
+{
+    public class IdentityClaimsProfileService : IProfileService
+    {
+        private readonly IUserClaimsPrincipalFactory<AppUser> _claimsFactory;
+        private readonly UserManager<AppUser> _userManager;
+
+        public IdentityClaimsProfileService(UserManager<AppUser> userManager, IUserClaimsPrincipalFactory<AppUser> claimsFactory)
+        {
+            _userManager = userManager;
+            _claimsFactory = claimsFactory;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            var principal = await _claimsFactory.CreateAsync(user);
+
+            context.IssuedClaims = principal.Claims.ToList();
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            context.IsActive = user != null;
+        }
+    }
+}
